d188b1043d242f3b31ca2e3dd06617a8ce913bcd
d02ev <vikramadityapratapsingh98@gmail.com>
2024-12-23 01:28:14 +0530

    added project constants and enums

---
+++ include/git_initializer/constants.h
@@ -0,0 +1,49 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+extern const std::string GIT_USER;
+extern const std::string GIT_KEY;
+extern const std::string LIST_IGNORES_ARG;
+extern const std::string LIST_AVAILABLE_CMDS_ARG;
+extern const std::string IGN_FLAG;
+extern const std::string USER_FLAG;
+extern const std::string KEY_FLAG;
+extern const std::string INIT_ARG;
+extern const std::string ADD_ARG;
+
+extern const std::string GIT_API_URL;
+extern const std::string GIT_HOME_BASE_URL;
+extern const std::string GIT_API_AUTH_HEADER_NAME;
+extern const std::string GIT_API_AUTH_HEADER_VALUE;
+extern const std::string GIT_API_ACCEPT_HEADER_NAME;
+extern const std::string GIT_API_ACCEPT_HEADER_VALUE;
+extern const std::string GIT_API_VERSION_HEADER_NAME;
+extern const std::string GIT_API_VERSION_HEADER_VALUE;
+extern const std::string GIT_CONTENT_TYPE_HEADER_NAME;
+extern const std::string GIT_CONTENT_TYPE_HEADER_VALUE;
+
+extern const std::string ERROR_CLR;
+extern const std::string INFO_CLR;
+extern const std::string SUCCESS_CLR;
+extern const std::string RESET;
+
+extern const std::string DEFAULT_IGN_FILES;
+extern const std::string IGN_FLAG_DEFAULT;
+extern const std::string IGN_FILE_NAME;
+extern const std::string README_FILE_NAME;
+extern const std::string STATIC_FILE_DIR_NAME;
+extern const std::string IGN_JSON_FILE_NAME;
+extern const std::string VALID_IGN_LIST_FILE_NAME;
+
+extern const std::string VALID_ARGS[4];
+extern const std::string VALID_FLAGS[3];
+
+enum class LogLevel {
+  Error,
+  Info,
+  Internal,
+  Success
+};
+#endif //CONSTANTS_H
---
+++ src/constants.cpp
@@ -0,0 +1,38 @@
+#include "git_initializer/constants.h"
+
+const std::string GIT_USER = "GIT_USER";
+const std::string GIT_KEY = "GIT_KEY";
+const std::string LIST_IGNORES_ARG = "lsig";
+const std::string LIST_AVAILABLE_CMDS_ARG = "lsc";
+const std::string IGN_FLAG = "--ig";
+const std::string USER_FLAG = "--user";
+const std::string KEY_FLAG = "--key";
+const std::string INIT_ARG = "init";
+const std::string ADD_ARG = "add";
+
+const std::string GIT_API_URL = "https://api.github.com/user/repos";
+const std::string GIT_HOME_BASE_URL = "https://github.com";
+const std::string GIT_API_AUTH_HEADER_NAME = "Authorization";
+const std::string GIT_API_AUTH_HEADER_VALUE = "Bearer";
+const std::string GIT_API_ACCEPT_HEADER_NAME = "Accept";
+const std::string GIT_API_ACCEPT_HEADER_VALUE = "application/vnd.github+json";
+const std::string GIT_API_VERSION_HEADER_NAME = "X-GitHub-Api-Version";
+const std::string GIT_API_VERSION_HEADER_VALUE = "2022-11-28";
+const std::string GIT_CONTENT_TYPE_HEADER_NAME = "Content-Type";
+const std::string GIT_CONTENT_TYPE_HEADER_VALUE = "application/json";
+
+const std::string ERROR_CLR = "\033[31m";
+const std::string INFO_CLR = "\033[32m";
+const std::string SUCCESS_CLR = "\033[32m";
+const std::string RESET = "\033[0m";
+
+const std::string DEFAULT_IGN_FILES = "dotenv,visualstudiocode,vs,linux";
+const std::string IGN_FLAG_DEFAULT = "dotenv,visualstudiocode,vs,linux";
+const std::string IGN_FILE_NAME = ".gitignore";
+const std::string README_FILE_NAME = "README.md";
+const std::string STATIC_FILE_DIR_NAME = ".git-initializer";
+const std::string IGN_JSON_FILE_NAME = "ignores.json";
+const std::string VALID_IGN_LIST_FILE_NAME = "valid_ignores_list.txt";
+
+const std::string VALID_ARGS[4] = {"lsig", "lsc", "init", "add"};
+const std::string VALID_FLAGS[3] = {"--ig", "--key", "--user"};
